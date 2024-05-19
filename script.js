fe8f9ebcbd009364baa6718182bb82f90aab1141
yihfei <lim.yih.fei@gmail.com>
2024-05-19 19:42:33 +0800

    Implement game logic

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const WINNING_COMBINATIONS = [
     [0, 4, 8],
     [2, 4, 6]
 ]
-
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const winningMessageElement = document.querySelector('.winning-message')
+const restartButton = document.getElementById('restart')
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.querySelector('#board')
 
@@ -18,11 +20,17 @@ let circleTurn
 
 startGame()
 
+restartButton.addEventListener('click', startGame)
+
 function startGame() {
     circleTurn = false
     cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick)
         cell.addEventListener('click', handleClick, { once: true })
     })
+    winningMessageElement.classList.remove('show')
 }
 
 function handleClick(e) {
@@ -30,12 +38,29 @@ function handleClick(e) {
     const currentClass = circleTurn ? CIRCLE_CLASS: X_CLASS
     placeMark(cell, currentClass)
     if (checkWin(currentClass)) {
-        console.log("winner")
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
     }
+}
 
-    swapTurns()
-    setBoardHoverClass()
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(CIRCLE_CLASS)
+    })
+}
 
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = `Draw!`
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
 }
 
 function placeMark(cell, currentClass) {
